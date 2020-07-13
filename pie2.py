f409fb53fc6a5f9fe3359bad61ec9c0f5d9feafd
Aliyah Geer <43519256+amg369@users.noreply.github.com>
2020-07-13 19:23:21 -0400

    Update pie2.py

--- pie2.py
+++ pie2.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
+# Difference between March COVID Case Numbers and June Case Numbers by State
 raw_data = {'States': ["California", "Florida", "Texas", "New York", "New Jersey", "Illinois", "Arizona", "Georgia",
                        "Massachusetts", "Pennsylvania"],
             'Mar': [8155, 6741, 3266, 83712, 22255, 5994, 1289, 4117, 6620, 4843],
@@ -12,6 +12,8 @@ raw_data = {'States': ["California", "Florida", "Texas", "New York", "New Jersey
 df = pd.DataFrame(raw_data, columns=['States', 'Mar', 'Apr', 'May', 'Jun'])
 color = [(.3, .2, .7), (.99, .5, .2), (.4, .7, .4), (.34, .45, .9), (.6, .33, .8), (.9, .4, .4), (.4, .7, .9),
          (.8, .17, .4), (.76, .63, .9), (.2, .4, .1)]
+
+# Make pie chart
 df['inc'] = df['Jun'] - df['Mar']
 plt.pie(df['inc'], labels=df['States'], colors=color)
 plt.show()
